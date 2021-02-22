bf1882036ba4d5955b651c069aac81821dff9a8c
nacong <nabinyang37@gmail.com>
2021-02-22 20:51:08 +0900

    main

---
+++ index.php
@@ -0,0 +1,14 @@
+<html>
+  <head>
+    <title>PHP test</title>
+  </head>
+  <body>
+    <h1>YANGNACONG</h1>
+    <ol>
+      <li><a herf="index.php?id=HACKING"HACKING</a></li>
+      <li><a herf="index.php?id=PICTURE"PICTURE</a></li>
+      <li><a herf="index.php?id=MOVIE"MOVIE</a></li>
+      <li><a herf="index.php?id=MUSIC"MUSIC</a></li>
+    </ol>
+  </body>
+</html>
